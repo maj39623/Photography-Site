1b6aa24e7ca2a7428df63e8f32098ca32452ac78
maj39623 <maj39623@gmail.com>
2022-03-31 09:06:46 -0400

    4. FAQ Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //Page Components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServiceSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />;
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
