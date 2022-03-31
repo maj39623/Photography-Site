4a4219fb168198a5615146922b7d706d790131db
maj39623 <maj39623@gmail.com>
2022-03-31 10:06:50 -0400

    6. styled components 2

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
 body{
     background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
 
 }
 
@@ -22,6 +23,7 @@ button{
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+    font-family: 'Inter', sans-serif;
     &:hover{
         background-color: #23d997;
         color: white;
@@ -36,6 +38,7 @@ h2{
     }
     h4{
         font-weight: bold;
+        font-size: 2rem;
     }
     span{
         font-weight: bold;
